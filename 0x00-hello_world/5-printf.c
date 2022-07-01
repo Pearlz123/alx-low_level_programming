031e9734bdad97eadfcf8a23b0e2f49e119bf440
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-01 13:37:22 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 /**
- * Main-prints a text exactly
- * Return-0 if exited properly, and non zero if otherwise 
+ * main - prints a text exactly
+ * Return: 0 if exited properly and non zero if otherwise 
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar,but the outcome is a piece of art\n");
 return (0);
 }
