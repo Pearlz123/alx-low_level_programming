84980fd3f66f73cbb069e8ea06fb3a4428a610e2
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-03 14:43:51 -0700

    new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * Return: 0 something
+ */
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122 ; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90 ; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
