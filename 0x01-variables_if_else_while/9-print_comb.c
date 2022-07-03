b55c7ce0d9b8bd000b58cab14fa0507fcf180f32
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-03 15:51:04 -0700

    new

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints all possible combinations of single-digit numbers.
+ * main - prints all possible combinations of single-digit numbers.
  * Return: returns 0 if successful
  */
 int main(void)
