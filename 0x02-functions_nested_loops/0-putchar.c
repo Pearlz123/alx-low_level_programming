e4abb47a3d574d06e9b5106d4d8da263ee72f6b5
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-05 04:20:30 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <strings.h>
+/**
+ * main - Write a program that prints _putchar
+ * Return: return 0 if successful
+ *
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
