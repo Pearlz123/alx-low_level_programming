af8af6c3376624be840570e9ac4e4938803763b3
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-05 14:22:41 -0700

    new

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,9 @@
  */
 int print_last_digit(int n)
 {
-int x;
-if (n < 0)
-n = -n;
-x = n % 10;
-_putchar(x + '0');
-return (x);
+int nac, lastdigit;
+lastdigit = n % 10;
+nac = lastdigit + 48;
+_putchar(nac);
+return (lastdigit);
 }
