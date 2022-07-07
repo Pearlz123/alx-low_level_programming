ac420f46250e5999268570862bdb056bb6a8ae3d
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-07 12:53:04 -0700

    new

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,21 +3,25 @@
 /**
  * print_line - draws a straight line
  * @n: drawing parameter
- * Return: straight line
- *
+ * Return: straight lines
  */
 
 void print_line(int n)
 {
-int a;
-if (n <= 0)
-{
-_putchar('\n');
-}
-for (a = 0; a <= n; a++)
-{
-_putchar('_');
-}
-_putchar('\n');
-}
+	int i;
 
+	i = 0;
+	while (i < n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	_putchar(95);
+	}
+	i++;
+}
+_putchar('\n');
+}
