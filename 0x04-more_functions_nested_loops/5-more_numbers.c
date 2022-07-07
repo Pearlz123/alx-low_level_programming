8cf1b15e7521137d4460d90f474220f4d4216f96
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-07 04:23:55 -0700

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers
+ */
+void more_numbers(void)
+{
+int a;
+int b;
+
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+}
