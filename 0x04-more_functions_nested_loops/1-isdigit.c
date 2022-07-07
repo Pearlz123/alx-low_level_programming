89e0f092a9d2fd23d5993b25b1b2aa5c914c12d4
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-07 03:58:18 -0700

    3 new files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - a function that checks for a digit
+ * @c: a parameter
+ * Return: 1 if digit or 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies integers
+ * @a: para 1
+ * @b: para 2
+ * Returns: returns a value of the multiplication
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
