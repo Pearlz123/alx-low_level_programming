f92e8d196b036de36e8eae84204f5b654efe8ac6
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-12 13:59:09 -0700

    new

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,14 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n; i++)
-		{
-			printf("%d", a[i]);
-			if (i != n - 1)
-				printf(", ");
-			else
-				break;
-		}
-	printf("\n");
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+printf(", ");
+else
+break;		}
+printf("\n");
 }
