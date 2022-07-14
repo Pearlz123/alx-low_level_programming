ae4d408f304336b67a8662a9901f81aa3f9ed265
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-14 13:35:43 -0700

    new

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - converts strings to upper case
+ * @str: str
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
