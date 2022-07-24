96efdd952bdd9b67b030b72fe1235f9d9e8a9e2e
Pearlz123 <comfort.ajani@stu.cu.edu.ng>
2022-07-23 17:47:51 -0700

    new

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: value to be checked.
+ * Return: 1 if c is uppercase.
+ */
+int _isupper(int c)
+{
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_memset - cant remember what it does
+ *@s: para 1
+ *@b: para 2
+ *@n: para 3
+ *Return: a value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{}
